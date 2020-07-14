efc0afad7edd5ae23918de69578750e09b41d7bc
John Chen <chenj317@gmail.com>
2020-07-14 13:43:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,7 +6,7 @@ puts "#{colors[0]}"
 
 def display_rainbow(rainbow)
   
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{color[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 
 display_rainbow(colors)
